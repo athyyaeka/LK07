9e21536c4b1be08dd9ce9fc91a18700cdd3e24b6
Athiya <athiyaez2007@gmail.com>
2026-05-12 09:00:12 +0700

    Refactor DuplicateNisException class structure

--- DuplicateNisException.java
+++ DuplicateNisException.java
@@ -1,21 +1,10 @@
 package LK07;
-
- /**
-  * Custom exception untuk menangani duplikasi NIS
-  */
 public class DuplicateNisException extends Exception {
-    
-    /**
-     * Constructor tanpa parameter
-     */
+
     public DuplicateNisException() {
         super("NIS sudah terdaftar di sistem!");
     }
 
-    /**
-     * Constructor dengan pesan custom
-     * @param message Pesan error
-     */
     public DuplicateNisException(String message) {
         super(message);
     }
